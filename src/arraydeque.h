b772dab48146cfbd042e8f4bba9e9bd67434fc26
DareDevilLuc <kscasirayan@up.edu.ph>
2026-03-14 10:50:04 +0800

    starting array deque

--- src/arraydeque.h
+++ src/arraydeque.h
@@ -1,1 +1,16 @@
+#include "../include/array.h"
+#include "../include/deque.h"
+#include "../include/list.h"
+#include <algorithm>
 
+template <typename T>
+class ArrayDeque : public Deque<T>, public List<T>{
+    private:
+        array<T> arr;
+        int j;
+        int size;
+    
+
+    public:
+        ArrayDeque() : a(10) {}
+};
