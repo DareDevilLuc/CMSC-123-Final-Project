7f0690b1ae780e26f2d793d5bb80edbaaa207544
Clandestine <clandestinemaglasang22@gmail.com>
2026-03-19 16:55:14 +0800

    made arraystack.h and fixed stack.h

--- include/stack.h
+++ include/stack.h
@@ -3,10 +3,9 @@
 
 template <typename T>
 class Stack{ //A stack follows the First-In, Last-Out (FILO) principle.
-
-    virtual void push(const T& x) = 0;  //Adds a new element to the top of the stack.
-    virtual T pop() = 0; //Removes and returns the top element from the stack and size decreases by 1.
-    virtual T top() const = 0; //Returns (but does not remove) the value of the top element without removing it.
-    virtual size_t size() const = 0; //Return the number of elements in the stack.
-    
+    public:
+        virtual void push(const T& x) = 0;  //Adds a new element to the top of the stack.
+        virtual T pop() = 0; //Removes and returns the top element from the stack and size decreases by 1.
+        virtual T top() const = 0; //Returns (but does not remove) the value of the top element without removing it.
+        virtual size_t size() const = 0; //Return the number of elements in the stack.
 };
--- src/arraystack.h
+++ src/arraystack.h
@@ -1,3 +1,100 @@
 #pragma once
 
+#include "../include/array.h"
+#include "../include/stack.h"
+#include "../include/list.h"
+#include <algorithm>
+#include <cstddef>
+#include <cassert>
 
+template <typename T>
+class ArrayStack : public Stack<T>, public List<T>{
+    private:
+        array<T> backingArray;
+        int n;
+
+    void resize(){
+        array<T> b(max(2 * this->n, 1));
+        for(int i = 0; i < this->n; i++){
+            b[i] = this->backingArray[i];
+        }
+        this->backingArray = b;
+    }
+
+    public:
+
+    ArrayStack() : backingArray(10), n(0){}
+
+    ~ArrayStack(){
+        delete[] this->backingArray.a;
+    }
+
+    size_t size() const override{
+        return this->n;
+    }
+
+    void add(const size_t i, const T& x) override{
+        assert(i >= 0 && i < this->n)
+        if(this->n >= this->backingArray.length){
+            resize();
+        }
+
+        for(int j = this->n; j >= i; j--){
+            this->backingArray[j] = this ->backingArray[j-1];
+
+        }
+        this->backingArray[i] = x;
+        this->n++;
+    }
+
+    T remove(const size_t i) override{
+        assert(i >= 0 && i < this->n);
+        T result = this->backingArray[i];
+        for(int j = i; j < n - 1; j++){
+            this->backingArray[j] = this->backingArray[j+1];
+        }
+        this->n--;
+
+        this->backingArray[this->n] = T{};
+
+        if(this->backingArray.length >= 3 * this->n){
+            this->resize();
+        }
+        return result;
+    }
+
+    T get(const size_t i) const override{
+        assert(i >=0 && i < this->n);
+        return this->backingArray[i];
+    }
+
+    T set(const size_t i, const T& x) override{
+        assert(i >=0 && i < this->n);
+        T y = this->backingArray[i];
+        this->backingArray[i] = x;
+        return y;
+    }
+
+    void push(const T& x) override{
+        if(this->n >= this->backingArray.length){
+            resize();
+        }
+        this->backingArray[n] = x;
+        this->n++;
+    }
+
+    T pop() override{
+        assert(i >=0 && i < this->n);
+        n--;
+        T result = this->backingArray[n];
+        this->backingArray[n] = T{};             
+        if(this->backingArray.length >= 3 * n){
+            resize();
+        }
+        return result;
+    }
+
+    T top() const override{
+        return this->backingArray[this->n - 1];
+    }
+};
