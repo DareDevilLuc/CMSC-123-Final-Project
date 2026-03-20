29f1115f8043e3e28a442ec69778ac8b88f17091
lmrkive <licastanos@up.edu.ph>
2026-03-20 17:36:59 +0800

    sorted set

--- include/sset.h
+++ include/sset.h
@@ -1,1 +1,17 @@
+#pragma once
+#define SSET
 
+template <typename T>
+
+class SortedSet {
+        public:
+            virtual ~SortedSet() {}
+
+            virtual bool add(T x ) = 0;
+
+            virtual bool remove(T x) = 0;
+
+            virtual bool hasElement(T x) = 0;
+
+            virtual int size() const = 0;
+};
