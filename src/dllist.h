f35e8e22f8d508ea857c976549136f51b3ac92b9
Clandestine <clandestinemaglasang22@gmail.com>
2026-03-20 16:53:35 +0800

    added dllist node and started dllist.h

--- src/dllist.h
+++ src/dllist.h
@@ -1,1 +1,27 @@
+//List ADT using Doubly Linked List
+#pragma once
 
+#include "../include/dlinkedlistnode.h"
+#include "../include/list.h"
+
+
+
+template <typename T>
+class DLList : public List<T>{
+    private:
+        Node<T> *head = nullptr;
+        Node<T> *tail = nullptr;
+        size_t n = 0;
+
+    public:
+        ~DLList(){}
+ 
+/*
+	virtual void add(const size_t i, const T& x) = 0;
+	virtual T remove(const size_t i) = 0;
+	virtual T get(const size_t i) const = 0;
+	virtual T set(const size_t i, const T& x) = 0;
+	virtual size_t size() const = 0;
+*/
+
+};
