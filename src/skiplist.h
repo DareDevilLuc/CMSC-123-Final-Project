c30c00e2c59d85d4431d7d7ea805aaa9eded25cd
lmrkive <licastanos@up.edu.ph>
2026-03-21 22:48:56 +0800

    updated readme and skiplist

--- src/skiplist.h
+++ src/skiplist.h
@@ -1,1 +1,94 @@
+#pragma once
+#include "../include/sset.h"
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
+template <typename T>
+class SkipList : public SortedSet<T> {
+private:
+    struct Node {
+        T data;
+        std::vector<Node*> forward;
+        Node(T val, int lvl) : data(val), forward(lvl + 1, nullptr) {}
+    };
+
+    Node* head;
+    int   maxHeight;
+    float prob;
+    int   count;
+
+    int randomLevel() {
+        int lvl = 0;
+        while ((static_cast<float>(rand()) / RAND_MAX) < prob && lvl < maxHeight)
+            lvl++;
+        return lvl;
+    }
+
+public:
+    SkipList(int maxLvl = 16, float p = 0.5f) : maxHeight(maxLvl), prob(p), count(0) {
+        srand(static_cast<unsigned>(time(0)));
+        head = new Node(T(), maxHeight);
+    }
+
+    ~SkipList() {
+        Node* cur = head;
+        while (cur) {
+            Node* tmp = cur;
+            cur = cur->forward[0];
+            delete tmp;
+        }
+    }
+
+    bool hasElement(T x) override {
+        Node* cur = head;
+        for (int i = maxHeight; i >= 0; i--)
+            while (cur->forward[i] && cur->forward[i]->data < x)
+                cur = cur->forward[i];
+        cur = cur->forward[0];
+        return cur && cur->data == x;
+    }
+
+    bool add(T x) override {
+        std::vector<Node*> predecessors(maxHeight + 1, nullptr);
+        Node* cur = head;
+        for (int i = maxHeight; i >= 0; i--) {
+            while (cur->forward[i] && cur->forward[i]->data < x)
+                cur = cur->forward[i];
+            predecessors[i] = cur;
+        }
+        cur = cur->forward[0];
+        if (cur && cur->data == x) return false;
+
+        int lvl = randomLevel();
+        Node* node = new Node(x, lvl);
+        for (int i = 0; i <= lvl; i++) {
+            node->forward[i]            = predecessors[i]->forward[i];
+            predecessors[i]->forward[i] = node;
+        }
+        count++;
+        return true;
+    }
+
+    bool remove(T x) override {
+        std::vector<Node*> predecessors(maxHeight + 1, nullptr);
+        Node* cur = head;
+        for (int i = maxHeight; i >= 0; i--) {
+            while (cur->forward[i] && cur->forward[i]->data < x)
+                cur = cur->forward[i];
+            predecessors[i] = cur;
+        }
+        cur = cur->forward[0];
+        if (!cur || cur->data != x) return false;
+
+        for (int i = 0; i <= maxHeight; i++) {
+            if (predecessors[i]->forward[i] != cur) break;
+            predecessors[i]->forward[i] = cur->forward[i];
+        }
+        delete cur;
+        count--;
+        return true;
+    }
+
+    int size() const override { return count; }
+};
--- tests/skiplist.cpp
+++ tests/skiplist.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include "../src/skiplist.h"
 
+using namespace std;
+using namespace chrono;
+
+int main() {
+    vector<int> sizes = {1000, 5000, 10000, 50000, 100000};
+
+    cout << left << setw(12) << "ListSize"
+         << setw(18) << "Avg add (us)"
+         << setw(18) << "Avg remove (us)"
+         << setw(18) << "Avg hasElement (us)" << endl;
+    cout << string(66, '-') << endl;
+
+    for (int n : sizes) {
+        double avgAdd, avgRemove, avgContains;
+
+        // --- Benchmark add ---
+        {
+            SkipList<int> sl;
+            auto start = high_resolution_clock::now();
+            for (int i = 0; i < n; i++) sl.add(i);
+            auto end = high_resolution_clock::now();
+            avgAdd = duration_cast<microseconds>(end - start).count() / double(n);
+        }
+
+        // --- Benchmark contains ---
+        {
+            SkipList<int> sl;
+            for (int i = 0; i < n; i++) sl.add(i); // pre-fill
+            auto start = high_resolution_clock::now();
+            for (int i = 0; i < n; i++) sl.hasElement(i);
+            auto end = high_resolution_clock::now();
+            avgContains = duration_cast<microseconds>(end - start).count() / double(n);
+        }
+
+        // --- Benchmark remove ---
+        {
+            SkipList<int> sl;
+            for (int i = 0; i < n; i++) sl.add(i); // pre-fill
+            auto start = high_resolution_clock::now();
+            for (int i = 0; i < n; i++) sl.remove(i);
+            auto end = high_resolution_clock::now();
+            avgRemove = duration_cast<microseconds>(end - start).count() / double(n);
+        }
+
+        cout << left << setw(12) << n
+             << setw(18) << fixed << setprecision(3) << avgAdd
+             << setw(18) << fixed << setprecision(3) << avgRemove
+             << setw(18) << fixed << setprecision(3) << avgContains
+             << endl;
+    }
+
+    return 0;
+}
